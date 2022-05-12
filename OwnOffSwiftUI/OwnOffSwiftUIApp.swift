13d9d98bed7e353ea43876ebcf4f07a65f5d4f69
dabdelwahed <dabdelwahed@mobelite.fr>
2022-05-12 15:54:28 +0100

    Initial Commit

---
+++ OwnOffSwiftUI/OwnOffSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  OwnOffSwiftUIApp.swift
+//  OwnOffSwiftUI
+//
+//  Created by Dorra Ben Abdelwahed on 12/5/2022.
+//
+
+import SwiftUI
+
+@main
+struct OwnOffSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
